45f7f63b1052c94c2f145ddfd522dbc35eb04b85
oner <xx@>
2023-10-14 22:53:20 +0800

    update sql

--- internal/models/todo.sql
+++ internal/models/todo.sql
@@ -2,7 +2,7 @@ create table task
 (
     id            bigint auto_increment
         primary key,
-    user_id       bigint                      not null,
+    user_id       int                      not null,
     type          int           default 0  not null comment 'task type  0',
     task_group_id int           default 0  not null,
     title         varchar(1000)            not null,
@@ -15,22 +15,24 @@ create table task
     updated_at    bigint        default 0  null
 );
 
+
 create table task_group
 (
-    id         bigint auto_increment
+    id         int auto_increment
         primary key,
     color      bigint       default 4294967295 not null comment '0xffffffff',
     icon       varchar(200) default ''         not null,
     name       varchar(100) default ''         not null,
     name_en    varchar(200) default ''         not null,
-    user_id    bigint          default 0          not null comment 'if who created new group then this colum will not be 0',
+    user_id    int          default 0          not null comment 'if who created new group then this colum will not be 0',
     created_at bigint       default 0          not null,
     updated_at bigint       default 0          not null
 );
 
+
 create table user
 (
-    id           bigint auto_increment
+    id           int auto_increment
         primary key,
     username     varchar(200)  default '' not null,
     email        varchar(200)  default '' not null,
@@ -43,7 +45,12 @@ create table user
     login_ip     varchar(100)  default '' not null,
     register_ip  varchar(100)  default '' not null,
     device_type  int           default 0  not null,
+    first_name   varchar(100)  default '' not null,
+    last_name    varchar(100)  default '' not null,
     constraint id_index
         unique (id)
 );
+
+create index user_email_index
+    on user (email);
 
